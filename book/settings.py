2f64f52585f8dbe8f212977df1886f2f2ff40757
Sanjarbek Rakhmonov <sanjarbek@kaist.ac.kr>
2020-05-29 19:51:22 +0900

    New commit

--- book/settings.py
+++ book/settings.py
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'book.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
