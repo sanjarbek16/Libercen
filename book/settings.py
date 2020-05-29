7e014bd59a11c5b9fe0024134581f24e4f102ca4
Sanjarbek Rakhmonov <sanjarbek@kaist.ac.kr>
2020-05-29 22:21:08 +0900

    domain change

--- book/settings.py
+++ book/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'libercen1.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'libercen1.herokuapp.com', 'libercen.xyz']
 
 
 # Application definition
